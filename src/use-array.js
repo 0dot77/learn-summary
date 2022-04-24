06c3ee83f7722f4cfc8df78b5e755657bcefc4d6
Taeyang Yoo <yty0706@gmail.com>
2022-04-24 14:38:30 +0900

    array-study-1

---
+++ src/use-array.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// q1. make a string out of an array
+
+const fruits = ["apple", "banana", "orange"];
+const fruitsToString = fruits.join(" ");
+console.log(fruitsToString);
+console.clear();
+
+// q2. q1
+
+const fruits2 = "사과, 바나나, 키위, 체리";
+const newFruits = fruits2.split(", ");
+console.log(newFruits);
+console.clear();
+
+// q3. make this array look like this : [5,4,3,2,1]
+const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // 5,4,3,2,1
+console.clear();
+
+// q4. make new array without the first two elements
+const array2 = [1, 2, 3, 4, 5];
+const newArray2 = array2.splice(2);
+console.log(newArray2); // 3,4,5
+console.clear();
+
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+
+const student = [
+  new Student("A", 29, true, 45),
+  new Student("B", 28, false, 80),
+  new Student("C", 30, true, 90),
+  new Student("D", 40, false, 66),
+  new Student("E", 18, true, 88)
+];
+
+// q5. find a student with the score 90
+const score90high = student.find((el) => el.score === 90);
+console.log(score90high);
+console.clear();
+
+// q6. make an array containing only the students' scores
+// results should be : [45,80,90,66,88]
+const resultScore = student.map((el) => el.score);
+console.log(resultScore); // [45,80,90,66,88]
+console.clear();
+
+// q8. check if there is a student with the score lower than 50
